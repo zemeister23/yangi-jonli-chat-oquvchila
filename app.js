21b95d4890fb03b5ce9296ddb9be908aa0d730f7
zemeister23 <sauda2332@gmail.com>
2019-08-13 12:27:54 +0000

    bower]

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
